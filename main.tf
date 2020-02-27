75198be5cc27ffcac5d96cc0e3604835c3b462f7
Baratnannaka <58674734+Baratnannaka@users.noreply.github.com>
2020-02-27 15:32:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
-
 provider "aws" {
-    region = "${var.region}"
+    region = var.region
 }
 
 resource "aws_vpc" "tf-vpc" {
-    cidr_block         = "${var.vpc_cidr}"
+    cidr_block         = var.vpc_cidr
     instance_tenancy    = "default"
 
     tags = {
@@ -13,13 +12,73 @@ resource "aws_vpc" "tf-vpc" {
     }
 }
 
+
+resource "aws_internet_gateway" "ig" {
+    vpc_id = aws_vpc.tf-vpc.id
+        tags = {
+        Name = var.IGW_name
+    }
+}
+
+
+
 resource "aws_subnet" "subnets" {
-    count        = "${length(data.aws_availability_zones.azs.names)}"
-    availability_zone = "${element(data.aws_availability_zones.azs.names,count.index)}" 
-    vpc_id       = "${aws_vpc.tf-vpc.id}"
-    cidr_block   = "${element(var.subnet_cidr,count.index)}"
+    count        = length(data.aws_availability_zones.azs.names)
+    availability_zone = element(data.aws_availability_zones.azs.names,count.index)
+    vpc_id       = aws_vpc.tf-vpc.id
+    cidr_block   = element(var.subnet_cidr,count.index)
 
     tags = {
         Name = "Subnet-${count.index+1}"
     }
 }
+
+
+
+
+
+
+resource "aws_route_table" "terraform-public" {
+    vpc_id = aws_vpc.tf-vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.ig.id
+    }
+
+    tags = {
+        Name = var.Main_Routing_Table
+    }
+}
+
+resource "aws_route_table_association" "terraform-public" {
+    count          = length(data.aws_availability_zones.azs.names)
+    subnet_id      = element(aws_subnet.subnets.*.id, count.index)
+    route_table_id = aws_route_table.terraform-public.id
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = var.aws_security_group
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.tf-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+
+data "aws_subnet_ids" "example" {
+  vpc_id = aws_vpc.tf-vpc.id
+}
+
